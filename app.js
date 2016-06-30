193bbcbdc4358f703e5fed280b16eef6dec9040a
Gabe Escobedo <escobedo.gabe@yahoo.com>
2016-06-30 16:15:21 -0500

    JavaScript Assignment

---
+++ app.js
@@ -0,0 +1,22 @@
+var x = 10;
+document.getElementById("demo").innerHTML = x;
+
+var x = 10;
+x += 5;
+document.getElementById("demo1").innerHTML = x;
+
+var x = 10;
+x -= 5;
+document.getElementById("demo2").innerHTML = x;
+
+var x = 10;
+x *= 5;
+document.getElementById("demo3").innerHTML = x;
+
+var x = 10;
+x /= 5;
+document.getElementById("demo4").innerHTML = x;
+
+var x = 10;
+x %= 5;
+document.getElementById("demo5").innerHTML = x;
